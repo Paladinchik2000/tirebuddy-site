40ac5b7f748a023fbb796237646f5737b8bfa213
Paladinchik <paladi.nikita2000@gmail.com>
2025-10-11 15:28:06 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import TireBuddyHome from "./components/TireBuddyHome";
 import AboutPage from "./components/AboutPage";
 import BlogPage from "./components/BlogPage";
@@ -11,11 +12,14 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<TireBuddyHome />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/blog" element={<BlogPage />} />
-        <Route path="/services" element={<ServicesPage />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<TireBuddyHome />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="blog" element={<BlogPage />} />
+          <Route path="services" element={<ServicesPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
 );
+
