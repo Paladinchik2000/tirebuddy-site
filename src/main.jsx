051ee93604336272a1cc53115d73e0bc6f36e24e
Paladinchik <paladi.nikita2000@gmail.com>
2025-10-11 14:38:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import TireBuddyHome from "./components/TireBuddyHome";
+import AboutPage from "./components/AboutPage";
+import BlogPage from "./components/BlogPage";
+import ServicesPage from "./components/ServicesPage";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<TireBuddyHome />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/services" element={<ServicesPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
