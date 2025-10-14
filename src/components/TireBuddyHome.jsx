869d1c8d241824b1af720d4d1f1a36525fc6ad17
Paladinchik <paladi.nikita2000@gmail.com>
2025-10-14 13:40:43 +0300

    Update TireBuddyHome.jsx

--- src/components/TireBuddyHome.jsx
+++ src/components/TireBuddyHome.jsx
@@ -32,6 +32,9 @@ export default function TireBuddyHome() {
               <p className="text-slate-700 max-w-xl mx-auto">
                 Fast, friendly, and mobile tire service across Ontario — from tire changes
                 to seasonal cleanup, we come to you.
+              </h1>
+              <p className="text-slate-700 max-w-xl mx-auto">
+                We offer a 10% discount for pensioner!
               </p>
             </section>
 
