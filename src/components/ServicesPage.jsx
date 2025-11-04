2581ce425a4f397a707ce5beddde33094adc1d8d
Paladinchik <paladi.nikita2000@gmail.com>
2025-11-04 19:16:30 +0200

    Update ServicesPage.jsx

--- src/components/ServicesPage.jsx
+++ src/components/ServicesPage.jsx
@@ -73,7 +73,6 @@ export default function ServicesPage() {
               >
                 Read More
               </Link>
-            </div>
             
         {/* ===== MOBILE TIRE CHANGE CARD ===== */}
 <div
