4ffd6fbd976cea20b9ccec8ff7d0cd91bf73fc76
Paladinchik <paladi.nikita2000@gmail.com>
2026-01-14 13:44:55 +0200

    Change Vite server host to '0.0.0.0'

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '11d02be2-30ed-4ada-a254-70d3f39d5201-00-gyl2skbu47gz.worf.replit.dev', // Часто нужно для облачных IDE
-    allowedHosts: true
+    host: '0.0.0.0', 
+    allowedHosts: true 
   }
 })
