b322ef07e30152cb3a419d501fb8b1cec2686a85
Paladinchik <paladi.nikita2000@gmail.com>
2026-01-14 13:39:58 +0200

    Enhance Vite config with server settings
    Add server configuration for Vite with custom host.

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({ plugins: [react()] })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '11d02be2-30ed-4ada-a254-70d3f39d5201-00-gyl2skbu47gz.worf.replit.dev', // Часто нужно для облачных IDE
+    allowedHosts: true
+  }
+})
